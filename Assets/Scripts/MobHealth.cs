d5e9b57859cf3dd0672691e90e6805008d1696ce
itzel505 <cruzgutierrez@chapman.edu>
2025-12-04 11:10:35 -0800

    made projectile for player/changed tile map

---
+++ Assets/Scripts/MobHealth.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class MobHealth : MonoBehaviour
+{
+    public int maxHealth = 50;
+    int currentHealth;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        Debug.Log("Mob took damage, new health: " + currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
+
